80985f21c3eebc61f12e8db883e09188a4812632
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2022-10-30 01:16:18 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/i2c_comm.elf.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Startup/startup_stm32f303k8tx.s" "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/cmake-build-debug/CMakeFiles/i2c_comm.elf.dir/Startup/startup_stm32f303k8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "EXTERNAL_CLOCK_VALUE=8000000"
+  "HSE_STARTUP_TIMEOUT=100"
+  "HSE_VALUE=8000000"
+  "HSI_VALUE=8000000"
+  "LSE_STARTUP_TIMEOUT=5000"
+  "LSE_VALUE=32768"
+  "LSI_VALUE=40000"
+  "PREFETCH_ENABLE=1"
+  "STM32F303x8"
+  "USE_FULL_LL_DRIVER"
+  "VDD_VALUE=3300"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F3xx_HAL_Driver/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F3xx/Include"
+  "../compass"
+  "../accelerometer"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_dma.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_dma.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_dma.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_exti.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_exti.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_exti.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_gpio.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_i2c.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_i2c.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_i2c.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_rcc.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_rcc.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_rcc.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usart.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usart.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usart.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_utils.c" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_utils.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_utils.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/dma.c" "CMakeFiles/i2c_comm.elf.dir/Src/dma.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/dma.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/gpio.c" "CMakeFiles/i2c_comm.elf.dir/Src/gpio.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/gpio.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/i2c.c" "CMakeFiles/i2c_comm.elf.dir/Src/i2c.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/i2c.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/main.c" "CMakeFiles/i2c_comm.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/main.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/stm32f3xx_hal_msp.c" "CMakeFiles/i2c_comm.elf.dir/Src/stm32f3xx_hal_msp.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/stm32f3xx_hal_msp.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/stm32f3xx_it.c" "CMakeFiles/i2c_comm.elf.dir/Src/stm32f3xx_it.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/stm32f3xx_it.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/syscalls.c" "CMakeFiles/i2c_comm.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/syscalls.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/sysmem.c" "CMakeFiles/i2c_comm.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/sysmem.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/system_stm32f3xx.c" "CMakeFiles/i2c_comm.elf.dir/Src/system_stm32f3xx.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/system_stm32f3xx.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/Src/usart.c" "CMakeFiles/i2c_comm.elf.dir/Src/usart.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/Src/usart.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/accelerometer/lsm6ds0.c" "CMakeFiles/i2c_comm.elf.dir/accelerometer/lsm6ds0.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/accelerometer/lsm6ds0.c.obj.d"
+  "C:/Users/DFUNDO/OneDrive/Documents/VRS/zadanie6_demo/vrs_cvicenie_6-demo_iks01a2/compass/lis3mdltr.c" "CMakeFiles/i2c_comm.elf.dir/compass/lis3mdltr.c.obj" "gcc" "CMakeFiles/i2c_comm.elf.dir/compass/lis3mdltr.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/i2c_comm.elf.dir/Src/i2c.c.obj.d
@@ -0,0 +1,30 @@
+CMakeFiles/i2c_comm.elf.dir/Src/i2c.c.obj: \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Src\i2c.c \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Inc/i2c.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Inc/main.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Device\ST\STM32F3xx\Include/stm32f3xx.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Device\ST\STM32F3xx\Include/stm32f303x8.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Include/core_cm4.h \
+ c:\progra~2\gnuarm~1\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\stdint.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\features.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\_newlib_version.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\_stdint.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Include/core_cmInstr.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Include/cmsis_gcc.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Include/core_cmFunc.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Include/core_cmSimd.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\CMSIS\Device\ST\STM32F3xx\Include/system_stm32f3xx.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_i2c.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_rcc.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_bus.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_system.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_exti.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_cortex.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_utils.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_pwr.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_dma.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_gpio.h \
+ C:\Users\DFUNDO\OneDrive\Documents\VRS\zadanie6_demo\vrs_cvicenie_6-demo_iks01a2\Drivers\STM32F3xx_HAL_Driver\Inc/stm32f3xx_ll_usart.h
