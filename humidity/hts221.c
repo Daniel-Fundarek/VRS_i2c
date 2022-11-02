5d647f9f8980619be3f996aae7fb2f1d98741858
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2022-11-02 09:45:23 +0100

    hts221 added init

--- humidity/hts221.c
+++ humidity/hts221.c
@@ -2,4 +2,77 @@
 // Created by DFUNDO on 10/30/2022.
 //
 
+#include <stdint-gcc.h>
 #include "hts221.h"
+
+
+static uint8_t address = HTS221_DEVICE_ADDRESS;
+
+//uint64_t lastMovementTick = 0;
+
+uint8_t hts221_read_byte(uint8_t reg_addr) {
+    uint8_t data = 0;
+    return *(i2c_master_read(&data, 1, reg_addr, address, 0));
+}
+
+void hts221_write_byte(uint8_t reg_addr, uint8_t value) {
+    i2c_master_write(value, reg_addr, address, 0);
+}
+
+void hts221_readArray(uint8_t *data, uint8_t reg, uint8_t length) {
+    i2c_master_read(data, length, reg, address, 0);
+}
+
+uint16_t hts221_init() {
+    uint16_t status;
+    int16_t val = hts221_read_byte(HTS221_WHO_AM_I);
+    if (val == HTS221_WHO_AM_I_VALUE) {
+        status = 1;
+    } else {
+        return 0;
+    }
+    // init registers
+    // reg1
+    uint8_t control1 = hts221_read_byte(HTS221_CONTROL_REG1);
+    control1 &= HTS221_RESET_CONTROL_REG1;
+    control1 |= HTS221_SET_PD_CONTROL_REG1;
+    control1 |= HTS221_SET_BDU_CONTROL_REG1;
+    control1 &= HTS221_7HZ_CONTROL_REG1;
+    hts221_write_byte(HTS221_CONTROL_REG1, control1);
+
+    /*uint8_t control2 = hts221_read_byte(HTS221_CONTROL_REG2);
+    control2 &= HTS221_RESET_CONTROL_REG2;
+    control2 |= HTS221_SET_BOOT_CONTROL_REG2;
+    hts221_write_byte(HTS221_CONTROL_REG2, control2);*/
+
+   // uint8_t control3 = hts221_read_byte(HTS221_CONTROL_REG3);
+
+
+    return status;
+
+}
+
+int16_t hts221_get_temp(float* x, float* y, float* z) {
+    uint8_t temp[2];
+    temp[0] = 0;
+    temp[1] = 1;
+    hts221_readArray(temp, HTS221_TEMP_OUT_H, 2);
+    //hts221_readArray(temp1,HTS221_TEMP_OUT_L,8);
+    uint16_t msbT0T1 = 0;
+    msbT0T1 = hts221_read_byte(0x35);
+    uint16_t T0_deg = hts221_read_byte(0x32);
+    uint16_t T1_deg = hts221_read_byte(0x33);
+    msbT0T1 &= 0xF;
+    uint16_t msbT0 = msbT0T1 & 0x3;
+    uint16_t calibrationT0 = msbT0 << 8;
+    calibrationT0 |= T0_deg;
+    uint16_t msbT1 = (msbT0T1 >> 2) & 0x3;
+    uint16_t calibrationT1 = msbT1 << 8;
+    calibrationT1 |= T1_deg;
+    uint16_t calibration = (calibrationT0 + calibrationT1) / 2 / 8;
+    *x = msbT0T1;
+    *y = temp[0];
+    *z = temp[1];
+
+}
+
--- humidity/hts221.h
+++ humidity/hts221.h
@@ -5,8 +5,14 @@
 #ifndef I2C_COMM_HTS221_H
 #define I2C_COMM_HTS221_H
 
+#include "i2c.h"
+
+
+#define HTS221_DEVICE_ADDRESS 0xBF
+//#define HTS221_DEVICE_WRITE_ADDRESS 0xBF
 
 #define HTS221_WHO_AM_I 0x0F
+#define HTS221_WHO_AM_I_VALUE 0xBC
 #define HTS221_AVERAGE_CONF 0x10
 
 #define HTS221_AVERAGE_CONF_TEMPERATURE 0x13
@@ -15,13 +21,35 @@
 #define HTS221_AVERAGE_CONF_HUMIDITY 0x10
 
 
+#define HTS221_CONTROL_REG1  0x20
 
-#define HTS221_CONTROL_REG1 0x20
+#define HTS221_RESET_CONTROL_REG1 ~(0x1<<7 | 0x7) // use =& to reset register
+#define HTS221_SET_PD_CONTROL_REG1 0x1<<7 // use |= to set PD
+#define HTS221_RESET_PD_CONTROL_REG1 ~0x1<<7 // use &= to reset PD
+
+#define HTS221_SET_BDU_CONTROL_REG1 0x1<<2 //use |= to set BDU
+#define HTS221_RESET_BDU_CONTROL_REG1 ~0x1<<2 //use &= to reset BDU
+
+#define HTS221_1HZ_CONTROL_REG1 0xFD  //use &= to set 1HZ temperature and humidity frequency
+#define HTS221_7HZ_CONTROL_REG1 0xFE //use &= to set 7HZ temperature and humidity frequency
+#define HTS221_12.5HZ_CONTROL_REG1 0xFF //use &= to set 12.5HZ temperature and humidity frequency
+
 
 #define HTS221_CONTROL_REG2 0x21
 
-#define HTS221_CONTROL_REG3 0x22
+#define HTS221_RESET_CONTROL_REG2 ~(0x1<<7 | 0x3) // use =& to reset register
+#define HTS221_SET_BOOT_CONTROL_REG2 0x1<<7 //use |= to set boot
+#define HTS221_RESET_BOOT_CONTROL_REG2 ~0x1<<7 //use &= to set boot
 
+#define HTS221_SET_HEATER_CONTROL_REG2 0x2 //use |= to set heater
+#define HTS221_RESET_HEATER_CONTROL_REG2 ~0x2 //use &= to reset heater
+
+#define HTS221_SET_ONESHOT_CONTROL_REG2 0x1 //use |= to set oneshot
+#define HTS221_SET_ONESHOT_CONTROL_REG2 ~0x1 //use &= to set oneshot
+
+//#define HTS221_RESET-control_REG2
+
+#define HTS221_CONTROL_REG3 0x22
 
 
 #define HTS221_STATUS_REG 0x27
@@ -30,11 +58,15 @@
 #define HTS221_TEMP_OUT_L 0x2A
 #define HTS221_TEMP_OUT_H 0x2B
 
+uint8_t hts221_read_byte(uint8_t reg_addr);
 
+void hts221_write_byte(uint8_t reg_addr, uint8_t value);
 
+void hts221_readArray(uint8_t *data, uint8_t reg, uint8_t length);
 
+uint16_t hts221_init();
 
-
+int16_t hts221_get_temp();
 
 
 #endif //I2C_COMM_HTS221_H
