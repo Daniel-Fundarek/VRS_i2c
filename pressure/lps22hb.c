30f700cc6d4aea58e1ba40a3d1a129a035f549b4
Daniel-Fundarek <xfundarekd@stuba.sk>
2022-11-15 17:27:08 +0100

    source and header file for sensor lps22hb created

---
+++ pressure/lps22hb.c
@@ -0,0 +1,8 @@
+/*
+ * lps22hb.c
+ *
+ *  Created on: Nov 15, 2022
+ *      Author: DFUNDO
+ */
+
+
---
+++ pressure/lps22hb.h
@@ -0,0 +1,13 @@
+/*
+ * lps22hb.h
+ *
+ *  Created on: Nov 15, 2022
+ *      Author: DFUNDO
+ */
+
+#ifndef LPS22HB_H_
+#define LPS22HB_H_
+
+
+
+#endif /* LPS22HB_H_ */
